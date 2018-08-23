7d368e2de59d4ef578b9c8595222035d62f55de9
Rafael Díaz <rafael.diaz@clearpeaks.com>
2018-08-24 00:42:58 +0200

    Added the scroll to bottom svg images, an animation on the arrow and resize to screen sizes minor of 600 pixels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { ScrollDownComponent } from './scroll-down/scroll-down.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    ScrollDownComponent
   ],
   imports: [
     BrowserModule
